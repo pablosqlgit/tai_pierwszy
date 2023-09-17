3b31b2be01624ee9dd8f9302bd72c3f4459081b2
kmielcarekgithub <krzysztof.mielcarek.tip19@zs1-swarzedz.pl>
2023-09-17 16:54:27 +0200

    working bmi calculator script
    oblicza i wyswietla bmi, bez css

--- src/components/BmiCalculator/BmiCalculator.jsx
+++ src/components/BmiCalculator/BmiCalculator.jsx
@@ -1,25 +1,45 @@
 import React, { useState } from 'react'
 
+const BmiResult = (props) =>{
+    const bmiValueCheck = () =>{
+        if (props.value < 18.5) {
+            return 'Masz niedowage, jedz więcej!';
+        } else if (props.value >= 18.5 && props.value < 24.9) {
+            return 'Twoja waga jest zdrowa, tak trzymaj!'
+        } else if (props.value >= 25.0 && props.value < 29.9) {
+            return 'Masz nadwage, schudnij!';
+        } else if (props.value >=30) {
+            return 'Masz otyłość, popracuj nad sobą!';
+        };
+    };
+    return <div>{`Twoje bmi wynosi: ${props.value}. ${bmiValueCheck()}`}</div>;
+}
+
 export default function BmiCalculator() {
-    let [genderValue, handleGenderChange] = useState();
     let [weightValue, handleWeightChange] = useState();
     let [heightValue, handleHeightChange] = useState();
+    const [isDivShown, setIsDivShown] = useState();
+    const bmiValue = (weightValue / (heightValue/100)**2).toFixed(2);
     const handleButtonClick = () =>{
-        console.log('przycisk dziala')
+        setIsDivShown(() => true)
     }
     return (
-    <div>
-        <span>Płeć: </span>
-        <input type="radio" name="gender" id="genderInput" onChange={(e)=>{handleGenderChange(genderValue = "female")}} />K
-        <input type="radio" name="gender" id="genderInput" onChange={(e)=>{handleGenderChange(genderValue = "male")}}/>M
-        <br/>
+        <div>
         <span>Waga: </span>
-        <input type="number" name="weight" id="weightInput" onChange={(e)=>handleWeightChange(weightValue = e.target.value)}/> kg
+        <input type="number" name="weight" id="weightInput" onChange={(e)=>handleWeightChange(
+            weightValue = e.target.value,
+            setIsDivShown(() => false)
+            )}/> kg
         <br/>
         <span>Wzrost: </span>
-        <input type="number" name="height" id="heightInput" onChange={(e)=>handleHeightChange(heightValue = e.target.value)}/> cm
+        <input type="number" name="height" id="heightInput" onChange={(e)=>handleHeightChange(
+            heightValue = e.target.value,
+            setIsDivShown(() => false)
+            )}/> cm
         <br/>
         <button onClick={handleButtonClick}>Oblicz</button>
+        {weightValue !== '' && heightValue !== '' && isDivShown && <BmiResult value={bmiValue}/>}
     </div>
   )
 }
+

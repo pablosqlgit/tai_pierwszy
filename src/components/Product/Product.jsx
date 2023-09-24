1e318c1f997702b271457e5c63ebeda3843b514b
kmielcarekgithub <krzysztof.mielcarek.tip19@zs1-swarzedz.pl>
2023-09-24 23:19:13 +0200

    last commit improvement
    ostatni commit doslownie rozwalal komputer wiec tu jest naprawa, wypisuje wszystkie potrzebne info na strone tylko trzeba teraz zrobic style

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -5,22 +5,42 @@ const Meal = (props) => {
 
   const { meal } = props
 
-  const recipeIngredientsList = [];
-  for(let i = 1; i< 20;i){
-    if (meal[`strIngredient${i}`] !== "" && meal[`strMeasure${i}`] !== "") {
-      recipeIngredientsList.push(`${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`)
-    }
-  }
-  console.log(recipeIngredientsList)
+  const showIngredients = () =>{
+    const measuredIngredients = [];
+    let x=1;
+    while(meal[`strIngredient${x}`]){
+        measuredIngredients.push(`${meal[`strIngredient${x}`]} - ${meal[`strMeasure${x++}`]}`);
+    };
+    const showedIngredients = measuredIngredients.map((ingredient) => <li>{ingredient}</li>);
+    return (
+      <ul>{showedIngredients}</ul>
+    );
+};
+console.log(meal)
 
   return (
     <>
-      <div>
-        {meal.strMeal}
+      <div id='product-container'>
+        <h1>
+          {meal.strMeal}
+        </h1>
+        <div id='recipe-wrapper'>
+          <div id='ingredients-wrapper'>
+            <span>Ingredients:</span>
+            {showIngredients()}
+          </div>
+          <div>
+            <img src={meal.strMealThumb}/>
+          </div>
+        </div>
+        <div id='product-socials-wrapper'>
+          {meal.strInstructions}
+        </div>
       </div>
     </>
   )
 }
+
 
 export default function Product() {
 

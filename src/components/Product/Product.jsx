cf08ca48e9a8ece73ff8a4d0b5860b6d12e53110
kmielcarekgithub <126149016+kmielcarekgithub@users.noreply.github.com>
2023-09-25 15:28:58 +0000

    Update Product.jsx

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,69 +1,62 @@
-import { React, useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { computeHeadingLevel } from "@testing-library/react";
+import { React, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 const Meal = (props) => {
+  const { meal } = props;
+  useEffect(() => {
+    document.title = meal.strMeal;
+  }, []);
 
-  const { meal } = props
-
-  const showIngredients = () =>{
+  const showIngredients = () => {
     const measuredIngredients = [];
-    let x=1;
-    while(meal[`strIngredient${x}`]){
-        measuredIngredients.push(`${meal[`strIngredient${x}`]} - ${meal[`strMeasure${x++}`]}`);
-    };
-    const showedIngredients = measuredIngredients.map((ingredient) => <li>{ingredient}</li>);
-    return (
-      <ul>{showedIngredients}</ul>
-    );
-};
-console.log(meal)
+    let x = 1;
+    while (meal[`strIngredient${x}`]) {
+      measuredIngredients.push(
+        `${meal[`strIngredient${x}`]} - ${meal[`strMeasure${x++}`]}`,
+      );
+    }
+    const showedIngredients = measuredIngredients.map((ingredient) => (
+      <li>{ingredient}</li>
+    ));
+    return <ul>{showedIngredients}</ul>;
+  };
+  console.log(meal);
 
   return (
     <>
-      <div id='product-container'>
-        <h1>
-          {meal.strMeal}
-        </h1>
-        <div id='recipe-wrapper'>
-          <div id='ingredients-wrapper'>
+      <div id="product-container">
+        <h1>{meal.strMeal}</h1>
+        <div id="recipe-wrapper">
+          <div id="ingredients-wrapper">
             <span>Ingredients:</span>
             {showIngredients()}
           </div>
           <div>
-            <img src={meal.strMealThumb}/>
+            <img src={meal.strMealThumb} />
           </div>
         </div>
-        <div id='product-socials-wrapper'>
-          {meal.strInstructions}
-        </div>
+        <div id="product-socials-wrapper">{meal.strInstructions}</div>
       </div>
     </>
-  )
-}
-
+  );
+};
 
 export default function Product() {
-
-  const [meal, setMeal] = useState()
-  const { productId } = useParams()
+  const [meal, setMeal] = useState();
+  const { productId } = useParams();
 
   useEffect(() => {
-    console.log(productId)
+    console.log(productId);
     fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${productId}`)
-    .then(res => res.json())
-    .then(res => setMeal({...res.meals[0]}))
-    .catch(err => console.log(err))
-  }, [])
+      .then((res) => res.json())
+      .then((res) => setMeal({ ...res.meals[0] }))
+      .catch((err) => console.log(err));
+  }, []);
 
   useEffect(() => {
-    console.log(meal)
-  }, [meal])
+    console.log(meal);
+  }, [meal]);
 
-  return (
-    <div>
-      {
-        !!meal ? <Meal meal={meal} /> : 'Loading...'
-      }
-    </div>
-  )
+  return <div>{!!meal ? <Meal meal={meal} /> : "Loading..."}</div>;
 }

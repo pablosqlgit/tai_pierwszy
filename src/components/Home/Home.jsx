c978415dfd47e21af5e8d745a1ab09a227b66fd1
Pablo <126149039+pablosqlgit@users.noreply.github.com>
2023-09-25 15:35:11 +0000

    Strona główna

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,103 +1,147 @@
-import { useEffect, React, useState, useReducer } from 'react'
-import { useLocation, useParams } from 'react-router-dom'
-import './Home.css'
-import CategoriesList from '../CategoriesList/CategoriesList'
-import ProductsList from '../ProductsList/ProductsList'
+import { useEffect, React, useState, useReducer } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import "./Home.css";
+import CategoriesList from "../CategoriesList/CategoriesList";
+import ProductsList from "../ProductsList/ProductsList";
 
 const initialState = {
   categories: [],
   products: [],
-}
+  filtered: "",
+};
 
-function reducer(prev = initialState, action){
-  switch(action.type){
-    case 'setCategories':
+function reducer(prev = initialState, action) {
+  switch (action.type) {
+    case "setCategories":
       return {
         ...prev,
-        categories: action.payload
-      }
-      break
-    case 'setProducts':
+        categories: action.payload,
+      };
+      break;
+    case "setProducts":
       return {
         ...prev,
-        products: action.payload
-      }
-      break
+        products: action.payload,
+      };
+      break;
+    case "setFiltered":
+      return {
+        ...prev,
+        filtered: action.payload,
+      };
+      break;
+    default:
+      return null;
+      break;
   }
 }
 
 export default function Home() {
-  const { search } = useLocation()
+  const { search } = useLocation();
 
-  const [state, dispatch] = useReducer(reducer, initialState)
+  const [state, dispatch] = useReducer(reducer, initialState);
 
-  const currentCategory = search.slice(3)
+  const currentCategory = search.slice(3);
 
   const getProducts = (which) => {
-    if(which === 'all'){
-      state.categories.forEach(category => {
-        fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${category.strCategory}`)
-        .then(res => res.json())
-        .then(res => {
-          const productsArray = state.products
-          res.meals.forEach(meal => {
-            productsArray.push(meal)
+    if (which === "all") {
+      state.categories.forEach((category) => {
+        fetch(
+          `https://www.themealdb.com/api/json/v1/1/filter.php?c=${category.strCategory}`,
+        )
+          .then((res) => res.json())
+          .then((res) => {
+            const productsArray = state.products;
+            res.meals.forEach((meal) => {
+              productsArray.push(meal);
+            });
+            dispatch({
+              type: "setProducts",
+              payload: productsArray,
+            });
           })
+          .catch((err) => {
+            console.log(err);
+          });
+      });
+    } else {
+      fetch(
+        `https://www.themealdb.com/api/json/v1/1/filter.php?c=${currentCategory}`,
+      )
+        .then((res) => res.json())
+        .then((res) => {
           dispatch({
-            type: 'setProducts',
-            payload: productsArray
-          })
+            type: "setProducts",
+            payload: res.meals,
+          });
         })
-        .catch(err => {
-          console.log(err)
-        })
-      })
-    }else{
-      fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${currentCategory}`)
-        .then(res => res.json())
-        .then(res => {
-          dispatch({
-            type: 'setProducts',
-            payload: res.meals
-          })
-        })
-        .catch(err => {
-          console.log(err)
-        })
+        .catch((err) => {
+          console.log(err);
+        });
     }
-  }
+  };
 
   useEffect(() => {
-    if(!!currentCategory){
-      getProducts(currentCategory)
-    }else{
-      getProducts('all')
+    if (!!currentCategory) {
+      getProducts(currentCategory);
+    } else {
+      getProducts("all");
     }
-  }, [state.categories])
+  }, [state.categories]);
 
   useEffect(() => {
-    document.title = 'Strona główna'
-    fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
-    .then(res => res.json())
-    .then(res => dispatch({ type: 'setCategories', payload: res.categories }))
-  }, [])
+    document.title = "Strona główna";
+    fetch("https://www.themealdb.com/api/json/v1/1/categories.php")
+      .then((res) => res.json())
+      .then((res) =>
+        dispatch({ type: "setCategories", payload: res.categories }),
+      );
+  }, []);
+
+  const handleSearch = (value) => {
+    const filtered = state.products.filter((meal) =>
+      meal.strMeal.toUpperCase().includes(value.trim().toUpperCase()),
+    );
+    if (!!value) {
+      dispatch({
+        type: "setFiltered",
+        payload: filtered,
+      });
+    } else {
+      dispatch({
+        type: "setFiltered",
+        payload: "",
+      });
+    }
+  };
 
   return (
-    <main className='home-page-wrapper'>
-      <div className='recipes-header-wrapper'>
-        <div className='searchbar-wrapper'>
+    <main className="home-page-wrapper">
+      <div className="recipes-header-wrapper">
+        <div className="searchbar-wrapper">
           <label>
-            <input type='text' className='search-bar' placeholder='Search Recipes'></input>
+            <input
+              type="text"
+              className="search-bar"
+              placeholder="Search Recipes"
+              onChange={(e) => handleSearch(e.target.value)}
+            ></input>
           </label>
         </div>
-        <div className='recipes-header'>
+        <div className="recipes-header">
           <h1>Explore recipes</h1>
         </div>
       </div>
-      <div className='recipes-wrapper'>
-        <CategoriesList categories={state.categories} currentCategory={currentCategory} />
-        <ProductsList products={state.products} />
+      <div className="recipes-wrapper">
+        <CategoriesList
+          categories={state.categories}
+          currentCategory={currentCategory}
+        />
+        <ProductsList
+          products={!!state.filtered ? state.filtered : state.products}
+          filtered={!!state.filtered ? true : false}
+        />
       </div>
     </main>
-  )
+  );
 }
--- src/components/ProductsList/ProductsList.jsx
+++ src/components/ProductsList/ProductsList.jsx
@@ -1,31 +1,27 @@
-import { React, useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import './ProductsList.css'
+import { React, useEffect } from "react";
+import { Link } from "react-router-dom";
+import "./ProductsList.css";
 
 export default function ProductsList(props) {
-
-  const { products } = props
-
-  useEffect(() => {
-    console.log(products)
-  })
-
+  const { products, filtered } = props;
 
   const productsList = products.map((product, key) => (
-    <div className='product' key={key}>
+    <div className="product" key={key}>
       <img src={product.strMealThumb} />
       <div>
         <p>{product.strMeal}</p>
         <Link to={`/product/${product.idMeal}`}>See product</Link>
       </div>
     </div>
-  ))
+  ));
 
   return (
-    <div className='products-list-wrapper'>
-      {
-        productsList
-      }
+    <div className="products-list-wrapper">
+      {!!productsList[0]
+        ? productsList
+        : filtered
+        ? "Nie ma takiego dania"
+        : "Wczytywanie..."}
     </div>
-  )
+  );
 }
